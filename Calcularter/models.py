8879848a55eec3e0d230038ad62afb43ddbcb821
Therapat <timeza86@gmail.com>
2020-02-24 15:00:52 +0700

    Create Model x y resuit

--- Calcularter/models.py
+++ Calcularter/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Varible_to_Calcular(models.Model):
+    x=models.IntegerField()
+    y=models.IntegerField()
+    resuit=models.IntegerField()
